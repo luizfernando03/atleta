8ae8ae391ee005e7c7f920e499395f1ea89ace33
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-02-16 11:59:43 -0300

    subindo logica

--- src/Atleta.java
+++ src/Atleta.java
@@ -6,6 +6,53 @@ public class Atleta {
     protected String cpf;
     private String dataNascimento;
 
+    public Atleta () {}
 
+    public Atleta(String nome, int quantidadeParticipacoes, String melhorTempo, String cpf, String dataNascimento) {
+        this.nome = nome;
+        this.quantidadeParticipacoes = quantidadeParticipacoes;
+        this.melhorTempo = melhorTempo;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeParticipacoes() {
+        return quantidadeParticipacoes;
+    }
+
+    public void setQuantidadeParticipacoes(int quantidadeParticipacoes) {
+        this.quantidadeParticipacoes = quantidadeParticipacoes;
+    }
+
+    public String getMelhorTempo() {
+        return melhorTempo;
+    }
+
+    public void setMelhorTempo(String melhorTempo) {
+        this.melhorTempo = melhorTempo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.lang.System;
 public class Main {
     public static void main(String[] args) {
 
         Atleta atleta = new Atleta();
+
+        atleta.setNome("Luiz Moura");
+        atleta.setQuantidadeParticipacoes(8);
+        atleta.setDataNascimento("03-11-1990");
+
+        System.out.println("Nome do atleta: " + atleta.getNome());
+        System.out.println("Quantidade de Participações: " + atleta.getQuantidadeParticipacoes());
+
+        System.out.println("fim do programa");
+
+
+
+
+
 
 
 
