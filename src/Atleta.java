61793f6a64da1b8f28a10281244789e18c752256
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2024-02-16 10:33:08 -0300

    colocando atributos

--- src/Atleta.java
+++ src/Atleta.java
@@ -1,2 +1,11 @@
 public class Atleta {
+
+    protected String nome;
+    protected int quantidadeParticipacoes;
+    protected String melhorTempo;
+    protected String cpf;
+    private String dataNascimento;
+
+
+
 }
